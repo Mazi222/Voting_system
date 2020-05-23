d7127ec474ffd6847887b4d0e9adebf99e2eb4e5
Patryk Bober <boberp26@gmail.com>
2020-05-23 02:38:09 +0200

    widok parametrów głosowania, layouty, przejścia między fragmentami

--- app/src/main/java/pl/edu/agh/votingapp/view/create/StepChooseTypeFragment.kt
+++ app/src/main/java/pl/edu/agh/votingapp/view/create/StepChooseTypeFragment.kt
@@ -2,59 +2,54 @@ package pl.edu.agh.votingapp.view.create
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.selection.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.stepstone.stepper.Step
 import com.stepstone.stepper.VerificationError
+import kotlinx.android.synthetic.main.fragment_step_choose_type.*
 import pl.edu.agh.votingapp.R
 import pl.edu.agh.votingapp.VotingType
 import pl.edu.agh.votingapp.viewmodel.create.CreateVotingViewModel
 
 
-class StepChooseTypeFragment : Fragment(R.layout.fragment_step_choose_type), Step {
+class StepChooseTypeFragment : Fragment(), Step {
 
-    private val adapter = MyListAdapter(this)
-    private var tracker: SelectionTracker<Long>? = null
     val model: CreateVotingViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val rv = RecyclerView(requireContext())
-        rv.layoutManager = LinearLayoutManager(context)
-        rv.adapter = adapter
+    ): View? =
+        inflater.inflate(R.layout.fragment_step_choose_type, container, false)
 
-        adapter.listData = VotingType.values()
-        adapter.notifyDataSetChanged()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        tracker = SelectionTracker.Builder(
-            "mySelection",
-            rv,
-            StableIdKeyProvider(rv),
-            MyItemDetailsLookup(rv),
-            StorageStrategy.createLongStorage()
-        ).withSelectionPredicate(
-            SelectionPredicates.createSelectSingleAnything()
-        ).build()
-
-        adapter.tracker = tracker
-
-        return rv
+        val fragment: StepChooseTypeFragment = this
+        choose_type.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter =
+                MyListAdapter(fragment, VotingType.values().filter { t -> t != VotingType.NONE })
+        }
     }
 
     override fun verifyStep(): VerificationError? {
         //return null if the user can go to the next step, create a new VerificationError instance otherwise
+        if (!model.isVotingTypeInitialized()) {
+            return VerificationError("Choose a voting type!")
+        }
         return null
     }
 
@@ -63,75 +58,51 @@ class StepChooseTypeFragment : Fragment(R.layout.fragment_step_choose_type), Ste
     }
 
     override fun onError(error: VerificationError) {
-        //handle error inside of the fragment, e.g. show error on EditText
+        Toast.makeText(
+            context,
+            error.errorMessage,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
-    class MyListAdapter(val fragment: Fragment) : RecyclerView.Adapter<MyViewHolder>() {
-
-        var listData: Array<VotingType> = arrayOf()
-        var tracker: SelectionTracker<Long>? = null
-
-        init {
-            setHasStableIds(true)
-        }
+    class MyListAdapter(
+        private val fragment: StepChooseTypeFragment,
+        private val listData: List<VotingType>
+    ) : RecyclerView.Adapter<MyViewHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
             val listItem = LayoutInflater.from(parent.context)
                 .inflate(R.layout.list_item, parent, false) as View
 
-            return MyViewHolder(
-                listItem
-            )
+            return MyViewHolder(listItem)
         }
 
         override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            tracker?.let {
-                holder.bind(listData[position], it.isSelected(position.toLong()), fragment)
-            }
+            val votingType: VotingType = listData[position]
+            holder.bind(fragment, votingType)
         }
 
         override fun getItemCount() = listData.size
-
-        override fun getItemId(position: Int): Long = position.toLong()
-
     }
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class MyViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
         private val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         private val descTextView: TextView = itemView.findViewById(R.id.descTextView)
-        private val relativeLayout: RelativeLayout = itemView.findViewById(R.id.relativeLayout)
 
-        fun bind(votingType: VotingType, isActivated: Boolean = false, fragment: Fragment) {
+        fun bind(fragment: StepChooseTypeFragment, votingType: VotingType) {
             titleTextView.text = votingType.type
             descTextView.text = votingType.description
-            itemView.isActivated = isActivated
-            relativeLayout.setOnClickListener { view ->
+
+            itemView.setOnClickListener { view ->
                 Toast.makeText(
                     view.context,
                     "Selected: " + votingType.type,
-                    Toast.LENGTH_LONG
+                    Toast.LENGTH_SHORT
                 ).show()
-
-//                val activity = fragment.activity as CreateVotingActivity
-//                activity.newVoting.type = votingType
+                fragment.model.votingType = votingType
+                itemView.isActivated = true
             }
-        }
-
-        fun getItemDetails(): ItemDetailsLookup.ItemDetails<Long> =
-            object : ItemDetailsLookup.ItemDetails<Long>() {
-                override fun getPosition(): Int = adapterPosition
-                override fun getSelectionKey(): Long? = itemId
-            }
-    }
-
-    class MyItemDetailsLookup(private val recyclerView: RecyclerView) :
-        ItemDetailsLookup<Long>() {
-        override fun getItemDetails(e: MotionEvent): ItemDetails<Long>? {
-            val view = recyclerView.findChildViewUnder(e.x, e.y)
-            if (view != null) {
-                return (recyclerView.getChildViewHolder(view) as MyViewHolder).getItemDetails()
-            }
-            return null
         }
     }
 
--- app/src/main/java/pl/edu/agh/votingapp/viewmodel/create/CreateVotingViewModel.kt
+++ app/src/main/java/pl/edu/agh/votingapp/viewmodel/create/CreateVotingViewModel.kt
@@ -1,8 +1,20 @@
 package pl.edu.agh.votingapp.viewmodel.create
 
 import androidx.lifecycle.ViewModel
-import pl.edu.agh.votingapp.Voting
+import pl.edu.agh.votingapp.VotingType
+import java.util.*
 
 class CreateVotingViewModel : ViewModel() {
-    var newVoting: Voting = Voting()
+    lateinit var votingType: VotingType
+    lateinit var content: String
+    lateinit var endTime: Date
+    var quorum: Int = 0
+    var numOfPeopleEntitled: Int = 1
+    var numOfPeopleToChoose: Int = 1
+    var isOpen: Boolean = false
+
+    fun isVotingTypeInitialized() = this::votingType.isInitialized
+    fun isContentInitialized() = this::content.isInitialized
+    fun isEndTimeInitialized() = this::content.isInitialized
+
 }
