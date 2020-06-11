4d435e2ea1c51c025ad1f1d4b15357094ca261ad
Patryk Bober <boberp26@gmail.com>
2020-06-12 00:10:49 +0200

    added broadcast receiver for wifi enabling/disabling

--- app/src/main/java/pl/edu/agh/votingapp/MainActivity.kt
+++ app/src/main/java/pl/edu/agh/votingapp/MainActivity.kt
@@ -1,14 +1,19 @@
 package pl.edu.agh.votingapp
 
 import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import pl.edu.agh.votingapp.receiver.WifiReceiver
 import pl.edu.agh.votingapp.view.create.CreateVotingActivity
 import pl.edu.agh.votingapp.view.list.ListVotingsActivity
 import pl.edu.agh.votingapp.view.vote.VotesListActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val wifiReceiver = WifiReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,9 @@ class MainActivity : AppCompatActivity() {
         listMyVotingsBtn.setOnClickListener {
             this.listMyVotings()
         }
+
+        val intentFilter = IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        applicationContext.registerReceiver(wifiReceiver, intentFilter)
     }
 
     private fun createVoting() {
@@ -45,4 +53,9 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+        applicationContext.unregisterReceiver(wifiReceiver)
+    }
 }
