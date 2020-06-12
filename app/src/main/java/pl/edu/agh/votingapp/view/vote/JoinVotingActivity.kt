6f620ec14096565ccd898e73b562b7cc2cc0635d
knyznar <katarzyna.nyznar@gmail.com>
2020-06-12 17:08:19 +0200

    [WIP] widok join

--- app/src/main/java/pl/edu/agh/votingapp/view/vote/JoinVotingActivity.kt
+++ app/src/main/java/pl/edu/agh/votingapp/view/vote/JoinVotingActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import pl.edu.agh.votingapp.R
 import pl.edu.agh.votingapp.viewmodel.join.ServerData
 import java.net.InetAddress
@@ -22,7 +24,9 @@ class JoinVotingActivity : AppCompatActivity() {
         val etFirstName: EditText = findViewById(R.id.etFirstName) as EditText
         val etLastName: EditText = findViewById(R.id.etLastName) as EditText
         val etUserCode: EditText = findViewById(R.id.etUserCode) as EditText
+        val tvVoteName: TextView = findViewById(R.id.tvVoteName) as TextView
 
+        tvVoteName.text = name
         val firstName = etFirstName.text
         val lastName = etLastName.text
         val userCode = etUserCode.text
@@ -34,11 +38,17 @@ class JoinVotingActivity : AppCompatActivity() {
 
         val joinVotingButton: Button = findViewById(R.id.btn_login)
         joinVotingButton.setOnClickListener {
-            Log.d("BallotBull: join voting", "Join voting with name ${name!!}")
-            val intent = Intent(this, AnswerListActivity::class.java)
-            ServerData.host = host
-            ServerData.port = port
-            startActivity(intent)
+            if (firstName.trim().isEmpty() || lastName.trim().isEmpty() || userCode.trim().isEmpty()) {
+                Toast.makeText(applicationContext, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d("BallotBull: join voting", "Join voting with name ${name!!}")
+                val intent = Intent(this, AnswerListActivity::class.java)
+                ServerData.host = host
+                ServerData.port = port
+                intent.putExtra("userName", "$firstName $lastName")
+                intent.putExtra("userCode", "$userCode")
+                startActivity(intent)
+            }
         }
     }
 }
