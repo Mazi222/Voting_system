39462c9c963880347af1e408015fefac8b291898
Patryk Bober <boberp26@gmail.com>
2020-06-14 23:28:06 +0200

    changed field label and automatic focus on add answer dialog opened

--- app/src/main/java/pl/edu/agh/votingapp/view/create/AddAnswerDialog.kt
+++ app/src/main/java/pl/edu/agh/votingapp/view/create/AddAnswerDialog.kt
@@ -1,11 +1,14 @@
 package pl.edu.agh.votingapp.view.create
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -27,6 +30,11 @@ class AddAnswerDialog(
 
     val model: CreateVotingViewModel by activityViewModels()
 
+    private lateinit var answerPictureImageView: ImageView
+    private lateinit var addPictureBtn: Button
+    private lateinit var answerContentLayout: TextInputLayout
+    private lateinit var addAnswerBtn: Button
+
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,12 +55,19 @@ class AddAnswerDialog(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val answerPictureImageView: ImageView = view.findViewById(R.id.answerPictureImageView)
-        val addPictureBtn: Button = view.findViewById(R.id.addPictureBtn)
-        val answerContentLayout: TextInputLayout = view.findViewById(R.id.answerContentAddLayout)
-        val addAnswerBtn: Button = view.findViewById(R.id.addAnswerBtn)
+        answerPictureImageView = view.findViewById(R.id.answerPictureImageView)
+        addPictureBtn = view.findViewById(R.id.addPictureBtn)
+        answerContentLayout = view.findViewById(R.id.answerContentAddLayout)
+        addAnswerBtn = view.findViewById(R.id.addAnswerBtn)
 
         answerPictureImageView.isVisible = false
+
+        Handler().postDelayed({
+            answerContentLayout.requestFocus()
+            val imm: InputMethodManager =
+                context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(answerContentLayout, InputMethodManager.SHOW_IMPLICIT)
+        }, 200)
 
         addAnswerBtn.setOnClickListener {
             Log.d(TAG, "Add answer button clicked in dialog")
